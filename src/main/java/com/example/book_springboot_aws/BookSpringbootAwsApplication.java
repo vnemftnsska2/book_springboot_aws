2dba1e16b572755d2282dad2208099151efa3ac6
tnsska2 <vnemftnsska2@naver.com>
2022-06-18 01:53:51 +0900

    Initial commit

---
+++ src/main/java/com/example/book_springboot_aws/BookSpringbootAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_springboot_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSpringbootAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSpringbootAwsApplication.class, args);
+    }
+
+}
